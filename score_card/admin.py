8b4bbead8ff14e6e5f9e71f9d340e70ec80e0d67
shashank-k-y <shashank.sky.8@gmail.com>
2022-08-26 16:13:54 +0530

    delte tables score and subject

--- score_card/admin.py
+++ score_card/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 from score_card import models
 
-admin.site.register(models.Subject)
-admin.site.register(models.Score)
+# admin.site.register(models.Subject)
+# admin.site.register(models.Score)
